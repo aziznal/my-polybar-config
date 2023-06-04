72fd25d2b6a9f5a9c342e15237084e9d988d14cc
Aziz <aziznal.dev@gmail.com>
2023-06-04 13:05:05 +0300

    setup initial polybar config

---
+++ launch.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# If all your bars have ipc enabled, you can use 
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
+
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+
+# Launch Default Bar
+# polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+
+# monitor_list=$(polybar --list-monitors | awk -F: '{print $1}')
+monitor_list="DP-0 HDMI-0"
+
+for m in $monitor_list; do
+    MONITOR=$m polybar --reload example &
+    sleep 1 # to make sure tray is displayed on the first bar
+done
+
+echo "Bars launched..."
